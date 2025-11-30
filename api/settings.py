3aacf3b9bceab44862753f254d3dffc0d9eada41
samyan <yzenithcorp@gmail.com>
2025-11-30 00:00:37 -0600

    add DATABASE_URL and install dj_database_url package

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +85,15 @@ DATABASES = {
         'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+# On Vercel (or anywhere you set DATABASE_URL), override with Neon Postgres
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,  # keep connections open / pooled
+    )
 
 
 # Password validation
